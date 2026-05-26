e54f6eb03ac3833a4da316307ef54a15455dde22
aurelgoh <aurelgoh@gmail.com>
2026-05-26 10:15:56 +0700

    Create withdraw.js

---
+++ pages/withdraw.js
@@ -0,0 +1,14 @@
+export default function Withdraw() {
+  return (
+    <div
+      style={{
+        background: "#040b2d",
+        minHeight: "100vh",
+        color: "white",
+        padding: "30px",
+      }}
+    >
+      <h1>Withdraw Page</h1>
+    </div>
+  );
+}
