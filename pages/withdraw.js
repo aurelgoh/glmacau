84808a3c36cb39800713b84ccb9449217dbfcfa0
aurelgoh <aurelgoh@gmail.com>
2026-05-26 10:45:02 +0700

    Update withdraw.js

--- pages/withdraw.js
+++ pages/withdraw.js
@@ -1,14 +1,58 @@
 export default function Withdraw() {
   return (
-    <div
-      style={{
-        background: "#040b2d",
-        minHeight: "100vh",
-        color: "white",
-        padding: "30px",
-      }}
-    >
-      <h1>Withdraw Page</h1>
+    <div style={styles.page}>
+      <h1>💸 Withdraw</h1>
+
+      <div style={styles.card}>
+        <h3>Available Balance</h3>
+        <h2>$1,250.00</h2>
+
+        <input
+          placeholder="Enter withdraw amount"
+          style={styles.input}
+        />
+
+        <button style={styles.button}>
+          Withdraw Now
+        </button>
+      </div>
     </div>
   );
 }
+
+const styles = {
+  page: {
+    background: "#040b2d",
+    minHeight: "100vh",
+    color: "white",
+    padding: "20px",
+    fontFamily: "Arial",
+  },
+
+  card: {
+    background: "#101c5d",
+    padding: "20px",
+    borderRadius: "24px",
+    marginTop: "20px",
+  },
+
+  input: {
+    width: "100%",
+    padding: "14px",
+    borderRadius: "14px",
+    border: "none",
+    marginTop: "16px",
+    marginBottom: "16px",
+  },
+
+  button: {
+    width: "100%",
+    padding: "16px",
+    border: "none",
+    borderRadius: "14px",
+    background: "#8e44ff",
+    color: "white",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+};
