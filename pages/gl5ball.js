f8dfd435b97c7bb0f2b68aa27ff67b840700d190
aurelgoh <aurelgoh@gmail.com>
2026-05-26 10:16:23 +0700

    Create gl5ball.js

---
+++ pages/gl5ball.js
@@ -0,0 +1,14 @@
+export default function GL5Ball() {
+  return (
+    <div
+      style={{
+        background: "#040b2d",
+        minHeight: "100vh",
+        color: "white",
+        padding: "30px",
+      }}
+    >
+      <h1>GL5 Ball</h1>
+    </div>
+  );
+}
