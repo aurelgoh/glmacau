49489d2f9daf5c500c0f0258ec041eeddeda64ac
aurelgoh <aurelgoh@gmail.com>
2026-05-27 09:39:36 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,18 @@
-import { useRouter } from "next/router";
-
 export default function Home() {
-  const router = useRouter();
-
-  const games = [
-    "GL5 Ball",
-    "Tencent 2 Ball",
-    "Tencent Car",
-    "Tencent of King",
-    "Lucky 5D",
-    "GL Pick Up",
-    "GL Racer",
-  ];
-
   return (
     <div
       style={{
-        minHeight: "100vh",
         background: "#020617",
+        minHeight: "100vh",
         color: "white",
-        padding: 40,
-        fontFamily: "Arial",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "50px",
+        fontFamily: "Arial"
       }}
     >
-      <h1
-        style={{
-          color: "#c084fc",
-          fontSize: 50,
-        }}
-      >
-        GL Macau
-      </h1>
-
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns:
-            "repeat(auto-fit,minmax(250px,1fr))",
-          gap: 20,
-          marginTop: 40,
-        }}
-      >
-        {games.map((game) => (
-          <div
-            key={game}
-            onClick={() =>
-              router.push(
-                `/game?name=${encodeURIComponent(game)}`
-              )
-            }
-            style={{
-              background: "#1e293b",
-              padding: 40,
-              borderRadius: 20,
-              cursor: "pointer",
-            }}
-          >
-            <h1>{game}</h1>
-          </div>
-        ))}
-      </div>
+      GL Macau
     </div>
   );
 }
